7d9cc034fc359bb2e2bdec0748c53056c5256465
Mo-Aziz <mohamed.aref32@gmail.com>
2022-08-09 11:20:37 -0500

    Finished styling, structuring the header, toggle menu functionality

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,39 @@
-import React from 'react'
-import './NavbarStyles.css'
+import React, { useState } from "react";
+import "./NavbarStyles.css";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
 
-export default Navbar
+    const [click, setClick]= useState()
+    const handleClick =()=>setClick(!click)
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="logo"><h1>DeFi-nance</h1></div>
+        <ul className={click ?"nav-menu active": "nav-menu"}>
+          <li>
+            {" "}
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Featured</a>
+          </li>
+          <li>
+            <a href="/">Earn</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+        </ul>
+        <div className="btn-group">
+          <button className="btn">Connect Wallet</button>
+        </div>
+        <div className="hamburger-icon" onClick={handleClick}>
+          {click? <FaTimes /> :<FaBars />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
